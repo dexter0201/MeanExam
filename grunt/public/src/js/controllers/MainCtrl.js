df7dbb00d0df72d2e53e9d9bf21df3e4e1bb2299
Lich Van Nguyen <lich.vannguyen@s60.com.au>
2014-12-05 16:39:10 +0700

    Add files

---
+++ grunt/public/src/js/controllers/MainCtrl.js
@@ -0,0 +1,19 @@
+// (function () {
+// 	'use strict';
+
+// 	var controllerID = 'mainController';
+// 	angular
+// 		.module('MainCtrl', []);
+// 		.controller(controllerID, refer);
+
+// 	refer.$inject = [];
+// 	function refer() {
+// 		var vm = this;
+
+// 		init();
+
+// 		function init() {
+// 			vm.message = 'Look at me go!';
+// 		}
+// 	};
+// })();
